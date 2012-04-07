c5fe96a787616d8bf2fe42c9ee571fd11d4c4f83
Adam Abdelhamed <adamabdelhamed@gmail.com>
2012-04-07 09:57:54 -0700

    Added NuGet spec and matching package.  Package is in the official NuGet Gallery.

--- PowerArgs/Properties/AssemblyInfo.cs
+++ PowerArgs/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PowerArgs")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The ultimate .NET command line parser")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Adam Abdelhamed")]
 [assembly: AssemblyProduct("PowerArgs")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("Copyright © Adam Abdelhamed 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
