fca013797c8d9bb23d9243bc8df41466cc747f69
Adam Abdelhamed <adamabdelhamed@gmail.com>
2012-04-13 08:58:15 -0700

    Fixed a broken test

--- ArgsTests/StickyArgTests.cs
+++ ArgsTests/StickyArgTests.cs
@@ -4,16 +4,19 @@ using System.Linq;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PowerArgs;
+using System.IO;
 
 namespace ArgsTests
 {
     [TestClass]
     public class StickyArgTests
     {
+        const string tempFile = @"C:\temp\stickyargs.txt";
+
         public class SampleArgs
         {
             public int NotSticky { get; set; }
-            [StickyArg]
+            [StickyArg(tempFile)]
             [ArgRequired]
             public int Sticky { get; set; }
         }
@@ -21,6 +24,10 @@ namespace ArgsTests
         [TestMethod]
         public void TestStickyArgs()
         {
+            var tempDir = Path.GetDirectoryName(tempFile);
+            if (Directory.Exists(tempDir) == false) Directory.CreateDirectory(tempDir);
+            if (File.Exists(tempFile)) File.Delete(tempFile);
+
             var args = new string[] { "-s", "12345" };
             Args.Parse<SampleArgs>(args);
             var remembered = Args.Parse<SampleArgs>(new string[0]);
