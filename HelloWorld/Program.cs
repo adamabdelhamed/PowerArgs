038a05f820825a13a75d3a3632d1baeb4146fc98
Adam Abdelhamed <adamabdelhamed@live.com>
2013-10-02 06:46:37 -0700

    reverted hello world sample

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -3,38 +3,14 @@ using PowerArgs;
 
 namespace HelloWorld
 {
-    public class BaseArgs
-    {
-        [ArgDescription("Run a sync cycle")]
-        [ArgActionMethod]
-        public static void Sync(SyncArgs syncArgs)
-        {
-            if (!syncArgs.Quiet)
-                Console.WriteLine("Sync called");
-            // [...] do work...
-        }
-    }
-
-    public class SyncArgs
-    {
-        [ArgShortcut("q")]
-        [ArgDescription("Suppress console output")]
-        public bool Quiet { get; set; }
-    }
-
-
     class Program
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Args.InvokeAction<BaseArgs>(args);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
+            // Samples.HelloWorldParse._Main(args);       //  The simplest way to use the parser.  All this sample does is parse the arguments and send them back to your program.
+            // Samples.HelloWorldInvoke._Main(args);      //  A simple way to have the parser parse your arguments and then call a new Main method that you build.
+             Samples.Git._Main(args);                   //  Sample that shows how to implement a program that accepts multiple commands and where each command takes its own set of arguments.
+            //Samples.REPLInvoke._Main(args);               //  Sample that shows how to implement a REPL (Read Evaluate Print Loop)
         }
     }
 }
