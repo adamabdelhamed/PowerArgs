24a877f1fd14fef415f617f1a6cc516291783417
Adam Abdelhamed <adamabdelhamed@live.com>
2016-09-05 18:37:51 -0700

    rev'd version

--- PowerArgs/Properties/AssemblyInfo.cs
+++ PowerArgs/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.7.1.0")]
-[assembly: AssemblyFileVersion("2.7.1.0")]
+[assembly: AssemblyVersion("2.7.2.0")]
+[assembly: AssemblyFileVersion("2.7.2.0")]
 //[assembly: InternalsVisibleTo("ArgsTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100599691571e51717eff0888f672ef5295866ae3749279076ba157e93eae2376a9867a2c1057b5e2f247d258de7b017e77c3ba5362ef2258f9af78e3a4a12dba4920b74df312454148da53396b7475e1e801ca42928296555f4a5dc90497ebae144680fb33bec22a86e4a7954bf66c94592c331a01f0e73c9b6db0b8e0ecb8fab1")]
 [assembly: InternalsVisibleTo("ArgsTests")]
