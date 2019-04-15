23d50e4854ea23124025dc288af0b1f02b638124
Adam Abdelhamed <adamabdelhamed@live.com>
2019-04-14 19:50:41 -0700

    Test for #139

---
+++ PowerArgsTestCore/Core/WhichShortcutTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerArgs;
+using System.Linq;
+
+namespace PowerArgsTestCore.Core
+{
+    public class WhichShortcutHook : ArgHook
+    {
+        public string ArgVal { get; set; }
+        public override void AfterPopulateProperty(HookContext context)
+        {
+            ArgVal = context.CmdLineArgs.Where(a => a.ToLower() == "-v" || a.ToLower() == "-vv").Single();
+        }
+    }
+
+    [TestClass]
+    public class WhichShortcutTest
+    {
+
+        [ArgShortcut("-v")]
+        [ArgShortcut("-vv")]
+        [WhichShortcutHook]
+        public bool Verbose { get; set; }
+
+
+        [TestMethod]
+        public void TestWhichShortcut()
+        {
+            var def = new CommandLineArgumentsDefinition(typeof(WhichShortcutTest));
+            var parsed = Args.Parse(def,"-vv");
+            var which = def.Arguments
+                .Where(a => a.DefaultAlias == nameof(Verbose))
+                .Single().Metadata
+                .WhereAs<WhichShortcutHook>().Single().ArgVal;
+            Assert.AreEqual("-vv", which);
+        }
+    }
+}
