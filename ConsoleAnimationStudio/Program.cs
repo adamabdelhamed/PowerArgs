eca25f876f7e0c41de463cf8b88d64ee81417067
Adam Abdelhamed <adamabdelhamed@live.com>
2021-03-13 04:01:43 -0800

    Add ConsoleAnimationStudio

---
+++ ConsoleAnimationStudio/Program.cs
@@ -0,0 +1,9 @@
+﻿using PowerArgs.Cli;
+
+namespace ConsoleAnimationStudio
+{
+    class Program
+    {
+        static void Main(string[] args) => new ConsoleApp(() => ConsoleApp.Current.LayoutRoot.Add(new ConsoleBitmapAnimationStudio()).Fill()).Run();
+    }
+}
