c659cfbb09333e20e0c2ad86d8b1413592273cb2
Adam Abdelhamed <adamabdelhamed@live.com>
2021-04-03 14:18:45 -0700

    Add built in fade filters

--- PowerArgs/CLI/Drawing/Filters/ColorFilter.cs
+++ PowerArgs/CLI/Drawing/Filters/ColorFilter.cs
@@ -1,4 +1,6 @@
-﻿namespace PowerArgs.Cli
+﻿using System.Threading.Tasks;
+
+namespace PowerArgs.Cli
 {
     public class ColorFilter : IConsoleControlFilter
     {
@@ -31,9 +33,115 @@
                     {
                         pixel.Value = new ConsoleCharacter(pixel.Value.Value, pixel.Value.ForegroundColor, Color);
                     }
-                    
                 }
             }
+        }
+    }
+
+    public class FadeOutFilter : IConsoleControlFilter
+    {
+        public float Percentage { get; set; }
+
+        /// <summary>
+        /// The control to filter
+        /// </summary>
+        public ConsoleControl Control { get; set; }
+
+        public void Filter(ConsoleBitmap bitmap)
+        {
+            for (var x = 0; x < bitmap.Width; x++)
+            {
+                for (var y = 0; y < bitmap.Height; y++)
+                {
+                    var pixel = bitmap.GetPixel(x, y);
+
+                    if (pixel.Value.BackgroundColor != pixel.Value.ForegroundColor && pixel.Value.BackgroundColor == RGB.Black && pixel.Value.Value != ' ')
+                    {
+                        pixel.Value = new ConsoleCharacter(pixel.Value.Value, pixel.Value.ForegroundColor.ToOther(RGB.Black, Percentage));
+                    }
+
+                    if (pixel.Value.BackgroundColor != RGB.Black)
+                    {
+                        pixel.Value = new ConsoleCharacter(pixel.Value.Value, pixel.Value.ForegroundColor, pixel.Value.BackgroundColor.ToOther(RGB.Black, Percentage));
+                    }
+                }
+            }
+        }
+    }
+
+    public class FadeInFilter : IConsoleControlFilter
+    {
+        public float Percentage { get; set; }
+
+        /// <summary>
+        /// The control to filter
+        /// </summary>
+        public ConsoleControl Control { get; set; }
+
+        public void Filter(ConsoleBitmap bitmap)
+        {
+            for (var x = 0; x < bitmap.Width; x++)
+            {
+                for (var y = 0; y < bitmap.Height; y++)
+                {
+                    var pixel = bitmap.GetPixel(x, y);
+
+                    if (pixel.Value.BackgroundColor != pixel.Value.ForegroundColor && pixel.Value.BackgroundColor == RGB.Black && pixel.Value.Value != ' ')
+                    {
+                        pixel.Value = new ConsoleCharacter(pixel.Value.Value, RGB.Black.ToOther(pixel.Value.ForegroundColor, Percentage));
+                    }
+
+                    if (pixel.Value.BackgroundColor != RGB.Black)
+                    {
+                        pixel.Value = new ConsoleCharacter(pixel.Value.Value, pixel.Value.ForegroundColor, RGB.Black.ToOther(pixel.Value.BackgroundColor, Percentage));
+                    }
+                }
+            }
+        }
+    }
+
+    public static class FadeEx
+    {
+        public static async Task FadeIn(this ConsoleControl c, float duration = 500, EasingFunction easingFunction = null)
+        {
+            easingFunction = easingFunction ?? Animator.Linear;
+            var filter = new FadeInFilter();
+            c.RenderFilters.Add(filter);
+
+            await Animator.AnimateAsync(new FloatAnimatorOptions()
+            {
+                From = 0, 
+                To = 1,
+                Duration = duration,
+                EasingFunction = (p)=> easingFunction(p),
+                Setter = p =>
+                {
+                    filter.Percentage = p;
+                    ConsoleApp.Current.Paint();
+                }
+            });
+            c.RenderFilters.Remove(filter);
+        }
+
+        public static async Task FadeOut(this ConsoleControl c, float duration = 500, EasingFunction easingFunction = null)
+        {
+            easingFunction = easingFunction ?? Animator.Linear;
+            var filter = new FadeOutFilter();
+            c.RenderFilters.Add(filter);
+
+            await Animator.AnimateAsync(new FloatAnimatorOptions()
+            {
+                From = 0,
+                To = 1,
+                Duration = duration,
+                EasingFunction = (p) => easingFunction(p),
+                Setter = p =>
+                {
+                    filter.Percentage = p;
+                    ConsoleApp.Current.Paint();
+                }
+            });
+            c.RenderFilters.Remove(filter);
         }
     }
 }
