71c20343ded3a29179e4741b10312ac41b756161
Adam Abdelhamed <adamabdelhamed@live.com>
2021-04-04 07:03:55 -0700

    add animation filter

--- PowerArgs/CLI/Drawing/Filters/ColorFilter.cs
+++ PowerArgs/CLI/Drawing/Filters/ColorFilter.cs
@@ -100,9 +100,65 @@ namespace PowerArgs.Cli
         }
     }
 
+    public class AnimationFilter : IConsoleControlFilter
+    {
+        public float Percentage { get; set; }
+        public RGB From { get; set; }
+        public RGB To { get; set; }
+
+        /// <summary>
+        /// The control to filter
+        /// </summary>
+        public ConsoleControl Control { get; set; }
+
+        public void Filter(ConsoleBitmap bitmap)
+        {
+            var targetColor = From.ToOther(To, Percentage);
+            for (var x = 0; x < bitmap.Width; x++)
+            {
+                for (var y = 0; y < bitmap.Height; y++)
+                {
+                    var pixel = bitmap.GetPixel(x, y);
+
+                    if (pixel.Value.BackgroundColor != pixel.Value.ForegroundColor && pixel.Value.BackgroundColor == RGB.Black && pixel.Value.Value != ' ')
+                    {
+                        pixel.Value = new ConsoleCharacter(pixel.Value.Value, targetColor);
+                    }
+
+                    if (pixel.Value.BackgroundColor != RGB.Black)
+                    {
+                        pixel.Value = new ConsoleCharacter(pixel.Value.Value, pixel.Value.ForegroundColor, targetColor);
+                    }
+                }
+            }
+        }
+    }
+
+
     public static class FadeEx
     {
-        public static async Task<IConsoleControlFilter> FadeIn(this ConsoleControl c, float duration = 500, EasingFunction easingFunction = null)
+        public static async Task<AnimationFilter> FadeAnimate(this ConsoleControl c, RGB from, RGB to, float duration = 500, EasingFunction easingFunction = null, float fromPerecntage = 0, float toPercentage = 1)
+        {
+            easingFunction = easingFunction ?? Animator.Linear;
+            var filter = new AnimationFilter() { From = from, To = to  };
+            c.RenderFilters.Add(filter);
+
+            await Animator.AnimateAsync(new FloatAnimatorOptions()
+            {
+                From = fromPerecntage,
+                To = toPercentage,
+                Duration = duration,
+                EasingFunction = (p) => easingFunction(p),
+                Setter = p =>
+                {
+                    filter.Percentage = p;
+                    ConsoleApp.Current.Paint();
+                }
+            });
+            return filter;
+        }
+
+        public static async Task<FadeInFilter> FadeIn(this ConsoleControl c, float duration = 500, EasingFunction easingFunction = null, float percentage = 1)
         {
             easingFunction = easingFunction ?? Animator.Linear;
             var filter = new FadeInFilter();
@@ -111,7 +167,7 @@ namespace PowerArgs.Cli
             await Animator.AnimateAsync(new FloatAnimatorOptions()
             {
                 From = 0, 
-                To = 1,
+                To = percentage,
                 Duration = duration,
                 EasingFunction = (p)=> easingFunction(p),
                 Setter = p =>
@@ -123,7 +179,7 @@ namespace PowerArgs.Cli
             return filter;
         }
 
-        public static async Task<IConsoleControlFilter> FadeOut(this ConsoleControl c, float duration = 500, EasingFunction easingFunction = null)
+        public static async Task<FadeOutFilter> FadeOut(this ConsoleControl c, float duration = 500, EasingFunction easingFunction = null, float percentage = 1)
         {
             easingFunction = easingFunction ?? Animator.Linear;
             var filter = new FadeOutFilter();
@@ -132,7 +188,7 @@ namespace PowerArgs.Cli
             await Animator.AnimateAsync(new FloatAnimatorOptions()
             {
                 From = 0,
-                To = 1,
+                To = percentage,
                 Duration = duration,
                 EasingFunction = (p) => easingFunction(p),
                 Setter = p =>
