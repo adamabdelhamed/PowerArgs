62971c9f59e0f8468033b84a81b23f997282670e
Adam Abdelhamed <adamabdelhamed@live.com>
2021-08-24 18:27:52 -0400

    Add MonthCalendar basic test

---
+++ PowerArgsTestCore/ConsoleApp/MonthCalendarTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerArgs;
+using PowerArgs.Cli;
+using System;
+using System.Collections.Generic;
+
+namespace ArgsTests.CLI.Controls
+{
+    [TestClass]
+    [TestCategory(Categories.ConsoleApp)]
+    public class MonthCalendarTests
+    {
+        public TestContext TestContext { get; set; }
+        
+        [TestMethod]
+        public void TestMonthCalendarBasicRender()
+        {
+            var app = new CliTestHarness(this.TestContext, 40, 20, true);
+            app.InvokeNextCycle(() => app.LayoutRoot.Add(new MonthCalendar(new MonthCalendarOptions() { Year = 2000, Month = 1 })).Fill());
+            app.InvokeNextCycle(async () =>
+            {
+                await app.Paint();
+                app.RecordKeyFrame();
+                app.Stop();
+            });
+            app.Start().Wait();
+            app.AssertThisTestMatchesLKG();
+        }
+    }
+}
