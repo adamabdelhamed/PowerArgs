6810ec44e943483e3c26e99f0eada230fd370dec
Adam Abdelhamed <adamabdelhamed@live.com>
2021-12-27 09:00:43 -0500

    Add support for custom, keyframed easing functions

--- PowerArgs/CLI/Animator.cs
+++ PowerArgs/CLI/Animator.cs
@@ -297,4 +297,31 @@ namespace PowerArgs.Cli
             }
         }
     }
+
+    public class CustomEase
+    {
+        private float[] keyFrames;
+        public CustomEase(float[] keyFrames)
+        {
+            this.keyFrames = keyFrames;
+        }
+
+        public float Ease(float percentage)
+        {
+            var rawIndex = (keyFrames.Length - 1) * percentage;
+            if (rawIndex == (int)rawIndex)
+            {
+                return keyFrames[(int)rawIndex];
+            }
+            else
+            {
+                var splitLocation = rawIndex - (int)rawIndex;
+                var previousKeyFrame = keyFrames[(int)rawIndex];
+                var nextKeyFrame = keyFrames[(int)rawIndex+1];
+                var nextFrameDelta = nextKeyFrame - previousKeyFrame;
+                var interpolationAmount = splitLocation * nextFrameDelta;
+                return previousKeyFrame + interpolationAmount;
+            }
+        }
+    }
 }
