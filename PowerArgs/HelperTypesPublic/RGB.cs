f88396b752bb88fd75ba8f5beae8d8f3e1d89d1c
Adam Abdelhamed <adamabdelhamed@live.com>
2022-09-28 16:37:43 -0400

    Prevent over and underflow in RGB.ToOther(). This will support animations that have easing functions that go below 0% or above 100%.

--- PowerArgs/HelperTypesPublic/RGB.cs
+++ PowerArgs/HelperTypesPublic/RGB.cs
@@ -217,6 +217,10 @@ public readonly struct RGB
         var g = G + dG * percentage;
         var b = B + dB * percentage;
 
+        r = r > 255 ? 255 : r < 0 ? 0 : r;
+        g = g > 255 ? 255 : g < 0 ? 0 : g;
+        b = b > 255 ? 255 : b < 0 ? 0 : b;
+
         return new RGB((byte)r, (byte)g, (byte)b);
     }
 
